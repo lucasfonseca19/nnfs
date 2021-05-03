c2069e0232708fea8333d190c46156adab00708c
lucasfonseca19 <62626721+lucasfonseca19@users.noreply.github.com>
2021-05-03 16:22:29 -0300

    ReLu class added and applied to the hidden layers

--- code.py
+++ code.py
@@ -14,12 +14,26 @@ class Layer_Dense:
     def foward(self, inputs):
         # Calculate output values from inputs, weights and biases
         self.output = np.dot(inputs, self.weights) + self.biases
+# Relu activation
+class Activation_ReLU:
+    def foward(self,inputs):
+        self.output = np.maximum(0,inputs)
 
 # Create dataset
 x,y = spiral_data(samples=100, classes=3)
+
 # Create Dense Layer with 2 input features and 3 output values
 dense1 = Layer_Dense(2,3)
+
+#Create ReLU activation (to be used with Dense Layer)
+activation1 = Activation_ReLU()
+
 # Perform foward pass of our training data through this layeer
 dense1.foward(x)
+
+# Foward pass throught the activation function
+# Takes in outputs from previous Layer
+activation1.foward(dense1.output)
+
 # Let's see the output of the first few samples
-print(dense1.output[:5])
+print(activation1.output[:5])
